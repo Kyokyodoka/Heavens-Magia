25ab622b1589e3704b29d917edb0603d664bb2d7
Swaggy-Bags <34581123+Trap-Master@users.noreply.github.com>
2022-01-10 19:51:22 -0500

    Stuff

---
+++ common/defines/magia.lua
@@ -0,0 +1,13 @@
+NDefines.NGame.START_DATE = "2150.1.1.1" -- Vanilla is 1936.1.1.1
+NDefines.NGame.END_DATE = "2200.1.1.1" -- Vanilla is 1949.1.1.1
+
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {400, 900, 1500}
+--NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {1000, 1100, 1500} --for modding only
+
+--Reduces Lag.
+
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 100
+
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 50
+
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 24
